1855271f99a8092403cc96917ef94f77a5a53338
uzifauzi <nurfauzi.dev@gmail.com>
2024-12-10 21:46:59 +0700

    refactor: modify header and section, created bottom navigation bar

---
+++ lib/widgets/custom_bottom_navbar.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+
+class CustomBottomNavbar extends StatelessWidget {
+  final int currentIndex;
+  final Function(int) onTap;
+
+  const CustomBottomNavbar({
+    super.key,
+    required this.currentIndex,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 76,
+      decoration: const BoxDecoration(
+        color: Colors.white70,
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(30),
+          bottomRight: Radius.circular(30),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black45,
+            offset: Offset(0, 20),
+            blurRadius: 20,
+          ),
+        ],
+      ),
+      child: ClipRRect(
+        borderRadius: const BorderRadius.only(
+          bottomLeft: Radius.circular(30),
+          bottomRight: Radius.circular(30),
+        ),
+        child: BottomNavigationBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: const Color(0xff18272a),
+          unselectedItemColor: const Color(0xff8b8c8b),
+          currentIndex: currentIndex,
+          onTap: onTap,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Iconsax.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Iconsax.search_normal),
+              label: 'Discover',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Iconsax.bookmark),
+              label: 'Bookmark',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Iconsax.profile_circle),
+              label: 'Profile',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/widgets/main_news_card.dart
+++ lib/widgets/main_news_card.dart
@@ -7,78 +7,81 @@ class MainNewsCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 405,
-      width: 315,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16),
-        image: const DecorationImage(
-          image: AssetImage('assets/vespa.jpg'),
-          fit: BoxFit.cover,
-        ),
-      ),
-      clipBehavior: Clip.hardEdge,
-      alignment: Alignment.bottomLeft,
+    return Padding(
+      padding: const EdgeInsets.only(left: 20),
       child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 23, vertical: 18),
+        height: 405,
+        width: 315,
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(16),
-          gradient: LinearGradient(
-            colors: [
-              Colors.black.withOpacity(0.7),
-              Colors.transparent,
-            ],
-            begin: Alignment.bottomCenter,
-            end: Alignment.topCenter,
+          image: const DecorationImage(
+            image: AssetImage('assets/vespa.jpg'),
+            fit: BoxFit.cover,
           ),
         ),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              padding:
-                  const EdgeInsets.symmetric(vertical: 8.0, horizontal: 20.0),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12.0),
-                  color: const Color(0xff182828).withOpacity(0.8)),
-              child: const Text(
-                "Business",
-                style:
-                    TextStyle(color: Colors.white, fontWeight: FontWeight.w500),
-              ),
-            ),
-            const Spacer(),
-            const Text(
-              "CNN Indonesia",
-              style:
-                  TextStyle(fontWeight: FontWeight.w500, color: Colors.white),
-            ),
-            const SizedBox(height: 4),
-            const Text("Vespa Navigation: Navigating The Scooter Scene",
-                style: TextStyle(
-                    fontWeight: FontWeight.w700, color: Colors.white)),
-            const SizedBox(height: 4),
-            Row(
-              children: [
-                const Text("8 min read - 2 hr ago",
-                    style: TextStyle(color: Colors.white)),
-                const Spacer(),
-                IconButton(
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.bookmark_border,
-                      color: Colors.white,
-                    )),
-                IconButton(
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.more_horiz,
-                      color: Colors.white,
-                    )),
+        clipBehavior: Clip.hardEdge,
+        alignment: Alignment.bottomLeft,
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 23, vertical: 18),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            gradient: LinearGradient(
+              colors: [
+                Colors.black.withOpacity(0.7),
+                Colors.transparent,
               ],
-            )
-          ],
+              begin: Alignment.bottomCenter,
+              end: Alignment.topCenter,
+            ),
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 8.0, horizontal: 20.0),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12.0),
+                    color: const Color(0xff182828).withOpacity(0.8)),
+                child: const Text(
+                  "Business",
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.w500),
+                ),
+              ),
+              const Spacer(),
+              const Text(
+                "CNN Indonesia",
+                style:
+                    TextStyle(fontWeight: FontWeight.w500, color: Colors.white),
+              ),
+              const SizedBox(height: 4),
+              const Text("Vespa Navigation: Navigating The Scooter Scene",
+                  style: TextStyle(
+                      fontWeight: FontWeight.w700, color: Colors.white)),
+              const SizedBox(height: 4),
+              Row(
+                children: [
+                  const Text("8 min read - 2 hr ago",
+                      style: TextStyle(color: Colors.white)),
+                  const Spacer(),
+                  IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.bookmark_border,
+                        color: Colors.white,
+                      )),
+                  IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.more_horiz,
+                        color: Colors.white,
+                      )),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
