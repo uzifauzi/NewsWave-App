8dcf599546c742d409822eef014b3e6ca2462c4c
uzifauzi <nurfauzi.dev@gmail.com>
2024-12-11 22:32:16 +0700

    feat: created list card news

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,31 +8,107 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
       child: Scaffold(
         backgroundColor: Color(0xfffcfffd),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            PageHeader(),
-            NewsSection(
-              title: "For You",
-            ),
-            SizedBox(height: 16),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: [
-                  MainNewsCard(),
-                  MainNewsCard(),
-                ],
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              PageHeader(),
+              NewsSection(
+                title: "For You",
               ),
-            ),
-            SizedBox(height: 21),
-            NewsSection(
-              title: "Following",
-            ),
-          ],
+              SizedBox(height: 16),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    MainNewsCard(),
+                    MainNewsCard(),
+                  ],
+                ),
+              ),
+              SizedBox(height: 21),
+              NewsSection(
+                title: "Following",
+              ),
+              SizedBox(height: 4),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Card(
+                  clipBehavior: Clip.antiAlias,
+                  color: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  child: Container(
+                    padding: EdgeInsets.all(8),
+                    height: 150,
+                    child: Row(
+                      children: [
+                        Expanded(
+                          flex: 3,
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(12),
+                            child: Image.asset(
+                              'assets/rapper.jpg',
+                              height: 96,
+                              width: 96,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 10),
+                        Expanded(
+                          flex: 7,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Row(
+                                children: [
+                                  SizedBox(width: 4),
+                                  Text(
+                                    'Alexia Virgie',
+                                    style: TextStyle(
+                                        fontSize: 12, color: Color(0xff18272a)),
+                                  ),
+                                  Spacer(),
+                                  IconButton(
+                                      onPressed: () {},
+                                      icon: const Icon(
+                                        Icons.bookmark_border,
+                                        color: Color(0xff18272a),
+                                      )),
+                                  IconButton(
+                                      onPressed: () {},
+                                      icon: const Icon(
+                                        Icons.more_horiz,
+                                        color: Color(0xff18272a),
+                                      )),
+                                ],
+                              ),
+                              Text(
+                                  'Fusion of Retro Glamour and Futuristic Trends',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w700,
+                                      fontSize: 16,
+                                      color: Color(0xff18272a))),
+                              SizedBox(height: 4),
+                              Text('8 min read • 2 hr ago',
+                                  style: TextStyle(color: Color(0xff18272a))),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 4),
+            ],
+          ),
         ),
       ),
     );
